f91db036931d05aa8109f666407e8d69dae2eb09
Ahmad <ahmad.codes.x@gmail.com>
2026-09-12 13:47:20 +0500

    Added the final App class with all menu and running logic. The program is wokring propelry for now proper edge case testing pedning

--- main.py
+++ main.py
@@ -97,12 +97,13 @@ class User():
         all_quizes = os.listdir('Quizes')
 
         paths = []
+        idx = 1
         for quiz in all_quizes:
-            idx = 1
+            
             path = f"Quizes/{quiz}"
             with open(path,'r') as f:
                 data = json.load(f)
-
+            
             if self.email in data['authors']:
                 print(f"{idx}: {data['title']}")
                 paths.append(path)
@@ -242,34 +243,157 @@ class Leaderboard():
         sorted_final = dict(sorted(final_data.items(), key=lambda item: item[1], reverse=True))
         return sorted_final
 
+
+class App():
+    def __init__(self):
+        self.current_user = None
+        self.is_running = True
+
+    def show_menu(self):
+        if self.current_user is None:
+            print("-------- Welcome To Quiz App ----------")
+            print("1: Signup")
+            print("2: Login")
+            print("3: Exit")
+        else:
+            print(f"--------- Hello {self.current_user.name} ---------")
+            print("1: Add Quiz")
+            print("2: Attempt Quiz")
+            print("3: Share Quiz")
+            print("4: Load Quiz from code")
+            print("5: See Leaderboard")
+            print("6: Logout")
+
+    def run(self):
+        while self.is_running:
+            self.show_menu()
+            choice = int(input("Enter your choice: "))
+            self.handle_choice(choice)
+
+    def handle_choice(self,choice):
+        if self.current_user is None:
+            if choice == 1:
+                self.handle_signup()
+            elif choice == 2:
+                self.handle_login()
+            elif choice == 3:
+                self.handle_exit()
+
+        else:
+            if choice == 1:
+                self.handle_add_quiz()
+            elif choice == 2:
+                self.handle_attempt_quiz()
+            elif choice == 3:
+                self.handle_share_quiz()
+            elif choice == 4:
+                self.handle_load_quiz()
+            elif choice == 5:
+                self.handle_show_leaderboard()
+            elif choice == 6:
+                self.handle_exit()
+
+    def handle_signup(self):
+        name = input("Enter Your name: ")
+        email = input("Enter your email: ")
+        pas = input("Create a strong password: ")
+
+        result = Registration.sign_up(name,email,pas)
+
+        if result:
+            print("Your account has been created successfully.")
+        else:
+            print("A user with this email already exist")
+
+    def handle_login(self):
+        email = input("Enter your email: ")
+        pas = input("Enter your password: ")
+
+        result = Registration.login(email,pas)
+
+        if result:
+            print("Login Successfull")
+            self.current_user = result
+        else:
+            print("Login Failed")
+
+    def handle_exit(self):
+        print("Thanks for using, Goodbye!")
+        self.is_running = False
+
+    def handle_add_quiz(self):
+        jason = input("Enter your json: ")
+        self.current_user.add_quiz(jason)
+        print("Quiz added successfully")
+
+    def handle_attempt_quiz(self):
+        self.current_user.attempt_quiz()
+
+    def handle_share_quiz(self):
+        self.current_user.share_quiz()
+
+    def handle_load_quiz(self):
+        result = self.current_user.load_quiz()
+
+        if result:
+            print("Quiz loaded successfully")
+        else:
+            print("Invalid code")
+
+    def handle_show_leaderboard(self):
+        print("------ Select the ranking category --------")
+        print("1: Rank based on points")
+        print("2: Rank based on no of quizzes attempt")
+        print("3: Rank based on overall percentage")
+
+        user = int(input("Enter your choice: "))
+
+        if user == 1:
+            result = Leaderboard.rank_total_score()
+        elif user == 2:
+            result = Leaderboard.rank_quizes_taken()
+        elif user == 3:
+            result = Leaderboard.rank_performance()
+        else:
+            print("Invalid Input given")
+
+        for user in result:
+            print(f"{user} : {result[user]}")
+
     
-# Registration.sign_up('Ahmad','ahmad@gmail.com','1122')
-# l = Registration.login('ahmad@gmail.com','1122')
+    
+    
+app = App()
 
-# l.share_quiz()
+app.run()
+
+# # Registration.sign_up('Ahmad','ahmad@gmail.com','1122')
+# # l = Registration.login('ahmad@gmail.com','1122')
+
+# # l.share_quiz()
 
 
-Registration.sign_up('Mustafa','mustafa@gmail.com',2222)
-l2 = Registration.login('mustafa@gmail.com',2222)
+# Registration.sign_up('Mustafa','mustafa@gmail.com',2222)
+# l2 = Registration.login('mustafa@gmail.com',2222)
 
-# r = l2.load_quiz()
-# print(r)
-
-l2.attempt_quiz()
-
-# # result = l2.attempt_quiz()
-# # print(result)
-
-# user = input("Enter jason: ")
-# l2.add_quiz(user)
+# # r = l2.load_quiz()
+# # print(r)
 
 # l2.attempt_quiz()
 
-# result = Leaderboard.rank_total_score()
-# result2 = Leaderboard.rank_quizes_taken()
+# # # result = l2.attempt_quiz()
+# # # print(result)
 
-# print(result)
-# print(result2)
+# # user = input("Enter jason: ")
+# # l2.add_quiz(user)
 
-# r = Leaderboard.rank_performance()
-# print(r)
+# # l2.attempt_quiz()
+
+# # result = Leaderboard.rank_total_score()
+# # result2 = Leaderboard.rank_quizes_taken()
+
+# # print(result)
+# # print(result2)
+
+# # r = Leaderboard.rank_performance()
+# # print(r)
